31ce5ef53ed1f179ddb73a48cc1850275e0dfade
Zee-code <zeeshanhiphopahmad@gmail.com>
2023-04-13 14:43:15 +0500

    changes

--- libs/reports/src/lib/daily-sale-report/daily-sale-report.component.ts
+++ libs/reports/src/lib/daily-sale-report/daily-sale-report.component.ts
@@ -1,19 +1,62 @@
+import { SharedHelpersFieldValidationsModule } from '@aims-pos/shared/helpers/field-validations';
+import { DatePipe } from '@angular/common';
+import { SharedServicesDataModule } from '@aims-pos/shared/services/data';
+import { SharedServicesGlobalDataModule } from '@aims-pos/shared/services/global-data';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'aims-pos-daily-sale-report',
   templateUrl: './daily-sale-report.component.html',
-  styleUrls: ['./daily-sale-report.component.scss']
+  styleUrls: ['./daily-sale-report.component.scss'],
 })
 export class DailySaleReportComponent implements OnInit {
-
-  currentDate: any = '';
+  // currentDate: any = '';
+  saleDate: any = '';
+  reportList: any = [];
+  lblTotalSale: any = '';
+  lblTotalCost: any = '';
+  lblTotalMargin: any = '';
 
   // rptImg: any = 'assets/ui/ReportPictures/Logo.svg'
-  constructor() { }
+  constructor(
+    private global: SharedServicesGlobalDataModule,
+    private dataService: SharedServicesDataModule,
+    private valid: SharedHelpersFieldValidationsModule,
+    private datePipe: DatePipe
+  ) {}
 
   ngOnInit(): void {
-    this.currentDate = new Date();
+    // this.currentDate = new Date();
   }
+  getDailySale() {
+    var date;
+    date = this.datePipe.transform(this.saleDate, 'yyyy-MM-dd');
+    this.dataService
+      .getHttp('report-api/FMISReport/getDailySales?invoiceDate=' + date, '')
+      .subscribe(
+        (response: any) => {
+          // this.deductionList = response;
+          this.reportList = response;
+          const sale = this.reportList.reduce((sum: any, total: any) => {
+            return sum + total.salePrice;
+          }, 0);
+          this.lblTotalSale = sale;
 
+          const cost = this.reportList.reduce((sum: any, total: any) => {
+            return sum + total.costPrice;
+          }, 0);
+          this.lblTotalSale = cost;
+
+          const margin = this.reportList.reduce((sum: any, total: any) => {
+            return sum + total.margin;
+          }, 0);
+          this.lblTotalMargin = margin;
+          // console.log(this.lblTotalSale);
+          // console.log(response);
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
+  }
 }
