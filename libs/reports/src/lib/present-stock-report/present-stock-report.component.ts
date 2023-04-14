147e3e8ca4ac021a3ff74713023e9c22d9d801b5
Zee-code <zeeshanhiphopahmad@gmail.com>
2023-04-14 12:22:20 +0500

    changes in present stock report

--- libs/reports/src/lib/present-stock-report/present-stock-report.component.ts
+++ libs/reports/src/lib/present-stock-report/present-stock-report.component.ts
@@ -1,20 +1,45 @@
+import { SharedHelpersFieldValidationsModule } from '@aims-pos/shared/helpers/field-validations';
+import { SharedServicesDataModule } from '@aims-pos/shared/services/data';
+import { SharedServicesGlobalDataModule } from '@aims-pos/shared/services/global-data';
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'aims-pos-present-stock-report',
   templateUrl: './present-stock-report.component.html',
-  styleUrls: ['./present-stock-report.component.scss']
+  styleUrls: ['./present-stock-report.component.scss'],
 })
 export class PresentStockReportComponent implements OnInit {
-
   dtpCurrentDate: any = '';
   cmbCategory: any = '';
-  
-  categoryList: any = [];
-  constructor() { }
+  reportDate: any = '';
+  stocktList: any = [];
+  constructor(
+    private global: SharedServicesGlobalDataModule,
+    private dataService: SharedServicesDataModule,
+    private valid: SharedHelpersFieldValidationsModule,
+    private datePipe: DatePipe
+  ) {}
 
   ngOnInit(): void {
-    this.dtpCurrentDate = new Date();
+    // this.dtpCurrentDate = new Date();
   }
+  getDailyStockReport() {
+    var date;
+    date = this.datePipe.transform(this.reportDate, 'yyyy-MM-dd');
+    this.dataService
+      .getHttp('report-api/FMISReport/getStockInStockOut?invDate=' + date, '')
+      .subscribe(
+        (response: any) => {
+          this.stocktList = response;
 
+          console.log(response);
+          // debugger;
+          // console.log(this.reportDate);
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
+  }
 }
