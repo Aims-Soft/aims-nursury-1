ca75bfb54c867a0b5ec2c1a171a9627bd2150e08
hammadhere12 <hammadnoor120@gmail.com>
2025-05-06 12:20:18 +0500

    changes of server IP

--- apps/aims-pos/src/environments/environment.prod.ts
+++ apps/aims-pos/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   envName: 'prod',
-  apiUrl: 'http://localhost:8500/',
-  // apiUrl: 'http://135.181.62.34:8500/',
+  // apiUrl: 'http://localhost:8500/',
+  apiUrl: 'http://159.69.174.28:8500/',
   // apiUrl: 'http://95.217.205.57:8500/',
   imageUrl: 'C:/inetpub/wwwroot/AimsNurseryProject/aims-pos/assets/ui/',
   imageSavedPath: 'http://localhost:7060/assets/ui/',
